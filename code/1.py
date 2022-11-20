c1147c5d67cecf7f7e10e3cf0bc185d683fd5950
Dylan Rogers <110286777+DylanRogers1@users.noreply.github.com>
2022-11-20 13:03:54 +0000

    Create 1.py

---
+++ code/1.py
@@ -0,0 +1,7 @@
+# Program 1 for textbook "The Ultimate Guide to Everything Python"
+# Author: Dylan Rogers
+# Website: http://123web.uk
+# 1 line of code
+# Copyright (c) Dylan Rogers
+
+print("Hello world")
